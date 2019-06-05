21ec2dda7cff076fc7bb4cdafd559315e7fd4eb7
loliconer <vuedev@163.com>
2019-06-05 17:49:37 +0800

    improve: Remove some plugins

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,9 @@
 import vue from 'rollup-plugin-vue'
-import serve from 'rollup-plugin-serve'
-import livereload from 'rollup-plugin-livereload'
 import resolve from 'rollup-plugin-node-resolve'
 import commonjs from 'rollup-plugin-commonjs'
 import less from 'rollup-plugin-less'
 
-const production = process.env.production, esm = process.env.esm
+const esm = process.env.esm
 let plugins = [
   resolve(),
   commonjs(),
@@ -24,16 +22,6 @@ if (esm) {
   format = 'esm'
   file = 'dist/NMPlayer.esm.js'
   name = undefined
-}
-
-if (!production) {
-  // plugins.push(serve('demo'))
-  plugins.push(livereload({
-    watch: 'demo',
-    delay: 100
-  }))
-  file = 'demo/js/NMPlayer.js'
-  format = 'iife'
 }
 
 export default {
